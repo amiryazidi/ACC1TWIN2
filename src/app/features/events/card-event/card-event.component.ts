8abc6af4a0b1e4f0c9a2bf35dca8ff0bdd9c361c
Amir YAZIDI <amir.yazidi1@esprit.tn>
2025-10-21 14:19:32 +0100

    bestEvent

---
+++ src/app/features/events/card-event/card-event.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+import { Eventy } from '../../../model/eventy';
+
+@Component({
+  selector: 'app-card-event',
+  templateUrl: './card-event.component.html',
+  styleUrl: './card-event.component.css'
+})
+export class CardEventComponent {
+@Input()  e:Eventy
+}
--- src/app/features/events/events.module.ts
+++ src/app/features/events/events.module.ts
@@ -5,13 +5,17 @@ import { EventsRoutingModule } from './events-routing.module';
 import { EventsComponent } from './events.component';
 import { ListEventComponent } from './list-event/list-event.component';
 import { DetailEventComponent } from './detail-event/detail-event.component';
+import { CardEventComponent } from './card-event/card-event.component';
+import { BestEventComponent } from './best-event/best-event.component';
 
 
 @NgModule({
   declarations: [
     EventsComponent,
     ListEventComponent,
-    DetailEventComponent
+    DetailEventComponent,
+    CardEventComponent,
+    BestEventComponent
   ],
   imports: [
     CommonModule,
